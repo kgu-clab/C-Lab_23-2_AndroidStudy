e25de46be39cbc9b0a9dc79ca57acd2fcbf281a6
Mariojung123 <idim06268@gmail.com>
2023-09-26 19:27:47 +0900

    Week2: 정의민 스터디 파일 제출
    Feat: 기본 문법 파일 제출
    -Kotlin 기본 문법 파일 작성 후 제출

---
+++ 정의민/Week 2/수업시간/Week2_pdm/src/main/kotlin/Main.kt
@@ -0,0 +1,142 @@
+fun main(args: Array<String>) {
+
+
+//    var age = 22
+//
+//    when(age){
+//        20 -> println("20살")
+//        22 -> println("당첨")
+//        30 -> println("꽝")
+//        else -> println("오류")
+//    }
+
+
+    //repeat은 중간에 break 불가능
+//    repeat(10){
+//
+//    }
+
+//    while(true){
+//
+//    }
+
+//    var list2 = List<Int>(10){1}
+//
+//    list2.forEach{
+//
+//    }
+
+//    for(i in 0 until 10){
+//        println(i)
+//    }
+//
+//    for(i in 0 .. 10){
+//        println(i)
+//    }
+//
+//    for(i in 0 .. 10 step 2){
+//        println(i)
+//    }
+//
+//    for(i in 0 downTo  10 ){
+//        println(i)
+//    }
+
+
+
+
+
+
+
+
+//    fun getName(name: String = "익명", age: Int): String = return "의민" + name + age
+//    fun getName(name: String = "익명", age: Int): String{
+//        return "의민" + name + age
+//    }
+//    println("${getName(age = 23)}")
+
+//    class info(
+//        val name: String,
+//        val age: Int,
+//        val height: Double,
+//        val weight: Double
+//    ){
+//        fun printinfo(){
+//            println("제 이름은 ${name}입니다. 나이는 ${age}입니다.")
+//        }
+//    }
+//
+//    val mario = info(name="정의민", age=23, height = 179.6, weight = 10.3)
+//    println(mario.age)
+
+
+//    mario.printinfo()
+
+
+
+
+
+
+
+
+
+
+
+
+////    va templist = List<Int>(size = 10){5} //배열 선언
+//
+////    val templist2: List<Int> = listOf()
+//
+//    val templist2: MutableList<Int> = mutableListOf() //어지간하면 val로 선언
+//
+////    templist2 = MutableList(size = 9){9}
+//
+//    templist2 += 1
+//    templist2 += 1
+//    templist2 += 1
+//
+//    templist2[0] = 3
+//
+//    println(templist2)
+
+
+
+
+/*    var name: String = "정의민" //변수는 var
+    var age =  23
+    var locate: String
+    val tag = "kottlin"
+    val tag2: String
+
+    tag2 = "hihi"
+    locate = "안녕"
+
+    age = 27
+
+    var Null: String? = "no"
+    println(Null)
+
+    Null = null
+    println(Null)
+
+    var Nullnum: Int? = 23
+
+    Nullnum = Nullnum!! + 3 //무조건 null이 아닌 경우만 사용
+
+    Nullnum = Nullnum ?: 0 + 3 //3항연산자느낌 null이면 0+3 아니면 Nullnum+3
+
+    Nullnum = null
+
+    println("제 이름은 ${name}입니다. 나이는 ${age*2}입니다")
+ */
+    //readln()은 무조건 String
+
+/*    var input = readln().toInt()
+    var input2 = readln().toInt()
+
+
+    println(input+input2)
+ */
+
+
+}
