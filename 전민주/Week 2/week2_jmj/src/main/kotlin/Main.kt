5d82c28504d9f52849e2237a4fc6fed2081fbe1e
mingmingmon <mingmingmon@naver.com>
2023-09-30 15:51:13 +0900

    Week 2: 전민주 과제 1,2,3 제출
    Week 2: 전민주 과제 1,2,3 제출

--- 전민주/Week 2/week2_jmj/src/main/kotlin/Main.kt
+++ 전민주/Week 2/week2_jmj/src/main/kotlin/Main.kt
@@ -1,56 +1,62 @@
+
 fun main(args: Array<String>) {
 
-    for(i in 0 .. 10){
+    for (i in 0..10) {
         println(i)
     }
 
-    for(i in 0 until 10){
+    for (i in 0 until 10) {
         println(i)
     }
 
-    for(i in 0 .. 10 step 2){
+    for (i in 0..10 step 2) {
 
     }
 
-    for(i in 10 downTo 1){
+    for (i in 10 downTo 1) {
 
     }
 
-    var list2 = List<Int>(10){1}
+    var list2 = List<Int>(10) { 1 }
 
-    list2.forEach{
-
-    }
-
-    repeat(10){//break 와 continue 불가
+    list2.forEach {
 
     }
 
-    while(true){
+    repeat(10) {//break 와 continue 불가
+
+    }
+
+    while (true) {
 
     }
 
     var age = 22
-    when(age){
+    when (age) {
         20 -> println("스무살")
         21 -> println("당첨")
         30 -> println("꽝")
         else -> println("오류")
     }
+}
 
-    /*var role = "role1"
+/*var role = "role1"
 
     when(role){
         "role1" ->
-    }*/
+    }*//*
 
-    /*fun getName(name: String = "익명", age: Int): String{
+
+    */
+/*fun getName(name: String = "익명", age: Int): String{
         return "minju" + name
     }
 
     println("${getName("이름", age = 22)}")
-*/
-    /*data class info(
+*//*
+
+    */
+/*data class info(
         val name: String,
         val age: Int,
         val height: Double,
@@ -63,9 +69,11 @@ fun main(args: Array<String>) {
 
     val MinJu = info(name = "전민주", age = 22, height = 163.2, weight = 55.0)
 
-    MinJu.printInfo()*/
+    MinJu.printInfo()*//*
 
-    /*var tempList: MutableList<Int> = mutableListOf(); // mutableList는 val로 짜기
+
+    */
+/*var tempList: MutableList<Int> = mutableListOf(); // mutableList는 val로 짜기
     //val로 설정한 애는 뭔가 할당되기 전까지는 할당가능. 한 번 할당 되면 그 뒤로는 재할당 불가
 
     tempList += 1;
@@ -73,14 +81,18 @@ fun main(args: Array<String>) {
     tempList += 1;
 
     tempList[0] = 3;
-    println(tempList)*/
+    println(tempList)*//*
 
-    /*var input1 = readln().toInt();
+
+    */
+/*var input1 = readln().toInt();
     var input2 = readln().toInt();
 
-    println(input1 + input2)*/
+    println(input1 + input2)*//*
 
-    /*println("Hello World!")
+
+    */
+/*println("Hello World!")
     var name = "전민주" // 변수는 무조건 var
     var age = 22 // 값 추론
     var locate: String // 아직 초기화는 안 했기 때문에 명시적으로 알려줌
@@ -95,6 +107,7 @@ fun main(args: Array<String>) {
 
     //NullNum = NullNum!! + 3
 
-    NullNum = NullNum ?: 0 + 3 // 앨비스 연산자?*/
+    NullNum = NullNum ?: 0 + 3 // 앨비스 연산자?*//*
 
-}
+
+}*/
