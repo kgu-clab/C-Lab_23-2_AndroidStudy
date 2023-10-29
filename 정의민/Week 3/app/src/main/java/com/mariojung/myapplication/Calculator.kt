8daf26747e8e5c12f2ec0f0628f0a01d16981885
Mariojung123 <idim06268@gmail.com>
2023-10-29 16:36:16 +0900

    Week3: 정의민 3번 과제 수정 파일 제출
    Style: 변수명 변경
    -변수명 변경은 하였는데 하면서도 이게 맞는지 잘 모르겠어서 일단 하긴했음.
    -노트북 문제인지 모르겠는데 AVD가 계속 실행중이라고 실행이 안됨

--- 정의민/Week 3/app/src/main/java/com/mariojung/myapplication/Calculator.kt
+++ 정의민/Week 3/app/src/main/java/com/mariojung/myapplication/Calculator.kt
@@ -1,6 +1,5 @@
 package com.mariojung.myapplication
 
-import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -8,59 +7,58 @@ import com.mariojung.myapplication.databinding.CalculatorBinding
 
 class Calculator : AppCompatActivity() {
     private lateinit var binding: CalculatorBinding
-    @SuppressLint("SuspiciousIndentation")
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) { //어노테이션 수정(삭제함)
         super.onCreate(savedInstanceState)
         binding = CalculatorBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.buttonPlus.setOnClickListener{
-            val Fnum = binding.editTextTextFirst.text.toString()
-            val Bnum = binding.editTextTextSec.text.toString()
+            val et_main_plus = binding.editTextTextFirst.text.toString()
+            val et_main_plus_sec = binding.editTextTextSec.text.toString()
 
-            val intFnum : Int = Fnum.toInt() // 첫번째 번호
-            val intBnum : Int = Bnum.toInt() // 두번째 번호
+            val int_et_main_plus : Int = et_main_plus.toInt() // 첫번째 번호
+            val int_et_main_plus_sec : Int = et_main_plus_sec.toInt() // 두번째 번호
 
-            val result : Int = intFnum + intBnum // 더하기 결과값
+            val result : Int = int_et_main_plus + int_et_main_plus_sec // 더하기 결과값
 
             val textView = binding.resultPoint
             textView.text = result.toString()
         }
 
         binding.buttonMinus.setOnClickListener{
-            val Fnum = binding.editTextTextFirst.text.toString()
-            val Bnum = binding.editTextTextSec.text.toString()
+            val et_main_minus = binding.editTextTextFirst.text.toString()
+            val et_main_minus_sec = binding.editTextTextSec.text.toString()
 
-            val intFnum : Int = Fnum.toInt() // 첫번째 번호
-            val intBnum : Int = Bnum.toInt() // 두번째 번호
+            val int_et_main_minus  : Int = et_main_minus.toInt() // 첫번째 번호
+            val int_et_main_minus_sec : Int = et_main_minus_sec.toInt() // 두번째 번호
 
-            val result : Int = intFnum - intBnum // 빼기 결과값
+            val result : Int = int_et_main_minus  - int_et_main_minus_sec // 빼기 결과값
 
             val textView = binding.resultPoint
             textView.text = result.toString()
         }
 
         binding.buttonMulty.setOnClickListener{
-            val Fnum = binding.editTextTextFirst.text.toString()
-            val Bnum = binding.editTextTextSec.text.toString()
+            val et_main_multy = binding.editTextTextFirst.text.toString()
+            val et_main_multy_sec = binding.editTextTextSec.text.toString()
 
-            val intFnum : Int = Fnum.toInt() // 첫번째 번호
-            val intBnum : Int = Bnum.toInt() // 두번째 번호
+            val int_et_main_multy : Int = et_main_multy.toInt() // 첫번째 번호
+            val int_et_main_multy_sec : Int = et_main_multy_sec.toInt() // 두번째 번호
 
-            val result : Int = intFnum * intBnum // 곱하기 결과값
+            val result : Int = int_et_main_multy * int_et_main_multy_sec // 곱하기 결과값
 
             val textView = binding.resultPoint
             textView.text = result.toString()
         }
 
         binding.buttonDiv.setOnClickListener{
-            val Fnum = binding.editTextTextFirst.text.toString()
-            val Bnum = binding.editTextTextSec.text.toString()
+            val et_main_div = binding.editTextTextFirst.text.toString()
+            val et_main_div_sec = binding.editTextTextSec.text.toString()
 
-            val intFnum : Int = Fnum.toInt() // 첫번째 번호
-            val intBnum : Int = Bnum.toInt() // 두번째 번호
+            val int_et_main_div : Int = et_main_div.toInt() // 첫번째 번호
+            val int_et_main_div_sec : Int = et_main_div_sec.toInt() // 두번째 번호
             val notion = "arguments are can not divided by 0"
-            if(intBnum == 0){
+            if(int_et_main_div_sec == 0){
                 Toast.makeText(
                     this,
                     notion,
@@ -68,7 +66,7 @@ class Calculator : AppCompatActivity() {
                 ).show()
             }
             else{
-                val result : Int = intFnum / intBnum // 나누기 결과값
+                val result : Int = int_et_main_div / int_et_main_div_sec // 나누기 결과값
 
                 val textView = binding.resultPoint
                 textView.text = result.toString()
